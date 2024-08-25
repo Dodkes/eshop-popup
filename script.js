51f95a00739f18adca4db40cd176f7aff430f205
Jozef_Zitt <dodo.zitt@gmail.com>
2024-08-25 22:58:12 +0200

    Popup form functionality

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const closeButton = document.querySelector("#close-button");
+const popup = document.getElementById("popup-container");
+const closeButton = document.getElementById("close-button");
+const popupShowTime = 2000;
 
 closeButton.addEventListener("click", () => {
-  alert("Close popup");
+  popup.style.display = "none";
+});
+
+onload = () => {
+  setTimeout(() => {
+    popup.style.display = "flex";
+  }, popupShowTime);
+};
+
+document.getElementById("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  console.log(name, email);
 });
